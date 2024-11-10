3edf5aa69970f62af5b2e828b2a4e1d80e93ab50
Vinh Vu <121175831+vivu-fadv@users.noreply.github.com>
2024-11-10 14:21:28 -0600

    Add get account by id

--- src/main/java/com/Banking/BankingSystem/Controller/AccountController.java
+++ src/main/java/com/Banking/BankingSystem/Controller/AccountController.java
@@ -3,7 +3,10 @@ package com.Banking.BankingSystem.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import com.Banking.BankingSystem.Model.Account;
 import com.Banking.BankingSystem.Repository.AccountRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // Allow Angular app
 @RequestMapping("/api/v1/")
 public class AccountController {
 	@Autowired
@@ -32,5 +36,14 @@ public class AccountController {
 	@PostMapping("/accounts")
 	public Account createAccount(@RequestBody Account account) {
 		return accountRepository.save(account);
+	}
+	
+	// get account by id rest api
+	@GetMapping("/accounts/{id}")
+	public ResponseEntity<Account> getAccountById(@PathVariable Integer id) {
+		Account account = accountRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account not exist with id: " + id));
+		
+		return ResponseEntity.ok(account);
 	}
 }
